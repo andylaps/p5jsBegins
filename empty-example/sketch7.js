451f5e894e26f988c49636d7e58d5d88b0e245d2
Andy Lapham <andylaps@gmail.com>
2016-08-29 12:21:58 +0100

    story so far
    It works but I don’t like the randomise arrow locations and the method
    I use is losing arrows as they flyback. Bit bored with this - will
    leave for a while.

--- empty-example/sketch7.js
+++ empty-example/sketch7.js
@@ -1,12 +1,12 @@
 var 	gWSpeed = 10, 
-		gWSrcGeoDeg, //degrees
-		gWDestGeoDeg;
+		gWSrcGeoDeg, // degrees
+		gWDestGeoDeg; // degrees
 
 // arrow dimensions		
-var	aTailHt = 60,
-		aHeadHt = 60,
-		aTailWd = 20,
-		aHeadWd = 40,
+var	aTailHt = 40,
+		aHeadHt = 40,
+		aTailWd = 11,
+		aHeadWd = 20,
 		aTotalHt = aTailHt+aHeadHt;
 // abstract coordinates to draw arrow around ctr of rotation (ctrRot 0,0)
 var arrowShape = [
@@ -19,8 +19,9 @@ var arrowShape = [
 		aTailWd/2, aTotalHt/2
 		];	
 
-var	gNoArrows = 5 ;
+var	gNoArrows = 3;
 		gSystem = [];
+		gFlybackMult = 150;
 
 function preload() {
 // Request data from openweathermap.org
@@ -31,20 +32,23 @@ function preload() {
 } // preload()		
 
 function setup() { 
-	// load data into global variables
+// load data into global variables
 	gWSrcGeoDeg = round(weather.wind.deg);
-	
-	// calculate angle for arrow to point = wind direction + 180deg
-	// use JS modulo (%) operator to limit rotation to 360deg
-	gWDestGeoDeg = (gWSrcGeoDeg+180) % 360;
-	// if (gWSrcGeoDeg+180 > 360) {
-	// 	gWDestGeoDeg = gWSrcGeoDeg 
-	// }	else	{
-	// 		gWDestGeoDeg = gWSrcGeoDeg+180
-	// };
-
 	gWSpeed = weather.wind.speed;
+// create vector from scalar speed
+	gWSpeedVector = p5.Vector.fromAngle(radians(gWSrcGeoDeg)+PI/2);
+
+// scale speed vector proportional to wind speed 	
+	gWSpeedVector.mult(gWSpeed);
+	gWFlybackVector = gWSpeedVector.copy();
+	gWFlybackVector.mult(gFlybackMult);
+	println("gWSpeedVector "+" "+gWSpeedVector);
+	println("gWFlybackVector "+gWFlybackVector);
 	println(weather.wind.deg+" "+weather.wind.speed);
+// calculate angle for arrow to point = wind direction + 180deg
+// use JS modulo (%) operator to limit rotation to 360deg
+	gWDestGeoDeg = (gWSrcGeoDeg+180) % 360;
+
 	// convert geo angle to P5 angle
 	// do it in stages for clarity
 	// not sure I need these anymore
@@ -55,15 +59,16 @@ function setup() {
 	// println("gWDestP5 "+gWDestP5);
 	createCanvas(800,	600);
 	// translate(width,height);
-// draw some lines for reference purposes	
-	background("grey");
-	stroke("green");
-	line(width/2,0,width/2,height);
-	stroke("red");
-	line(0,height/2,width,height/2);
-	stroke("green");
-	line(-width/2,100,width,100);
+// // draw some lines for reference purposes	
+// 	background("grey");
+// 	stroke("green");
+// 	line(width/2,0,width/2,height);
+// 	stroke("red");
+// 	line(0,height/2,width,height/2);
+// 	stroke("green");
+// 	line(-width/2,100,width,100);
 	noStroke();
+	fill(255,102)
 // spawn arrows proportional to wind speed (wSpeed)	
 	for (var i = 0; i < gNoArrows; i++) {
 		gSystem[i] = new Arrow(i);
@@ -85,37 +90,53 @@ function Arrow(a) { // arrow constructor
 	this.turn = 0;
 	this.fullGrown = false;
 	this.fullTurn = false;
-
-	this.vel = createVector(gWSpeed,gWSpeed);
-	// this.pos = createVector(200+a*50,200+a*50);
-	this.pos = createVector(random(0,width-aTotalHt),random(0,height-aTotalHt));
-
-	// need to correct overrun
-	this.update = function() {
-	// rotate arrow to point FROM source = wind direction + 180deg
-	if (this.fullTurn == false){
-		// this.turn = this.turn+1;	
-		if (this.turn == gWDestGeoDeg) {
-		this.fullTurn = true;
-		println(this.turn);
-		}	
-		this.turn = this.turn+1;
-		return; 		
+// not strictly necessary to use this.vel - here for completeness
+	this.vel = gWSpeedVector.copy();
+	this.flyback = gWFlybackVector.copy();
+	
+	if (a % 5 == 0) {
+		this.colString = "red";
+	} 	else { 
+		this.colString = "white";
 	}
-	this.pos.add(this.vel);
-	} 
+	this.pos = createVector(random(0,width),random(0,height));
+	println("this.pos "+this.pos);
+	println("this.vel "+this.vel);
+	println("this.flyback "+this.flyback);
+	
+	// this.pos = createVector(200+a*50,200+a*50);
+	
+	
 
+	this.update = function() {
+// rotate arrow to point FROM source = wind direction + 180deg
+		if (this.fullTurn == false){
+			if (this.turn == gWDestGeoDeg) {
+			this.fullTurn = true;
+			println(this.turn);
+			}	
+			this.turn = this.turn+1;
+			return; // don't do anything else until rotate is complete
+		}
+		if (this.pos.x < 0 || this.pos.y < 0 ) {
+			this.pos.sub(this.flyback);
+		}	else {
+			this.pos.add(this.vel);
+		}
 
+	}
+		
 	this.show = function() {
-	push();	
-	translate(this.pos.x,this.pos.y);
-	rotate(radians(this.turn));
-	beginShape();
-		for (var i = 0; i<arrowShape.length; i+=2) {
-		vertex(arrowShape[i], arrowShape[i+1]);
-		}; // for
-	endShape(CLOSE);
-	pop();
+		push();
+		fill(this.colString);	
+		translate(this.pos.x,this.pos.y);
+		rotate(radians(this.turn));
+		beginShape();
+			for (var i = 0; i<arrowShape.length; i+=2) {
+			vertex(arrowShape[i], arrowShape[i+1]);
+			}; // for
+		endShape(CLOSE);
+		pop();
 	} // show
 
 
